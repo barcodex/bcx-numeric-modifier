5656080b77773ff39f799d58a09e970991655d03
Anvarzon Zurajev <barcodex@gmail.com>
2015-03-30 17:52:01 +0200

    =first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+exports.apply = apply;
+
+/*
+value   - base value to modify
+name    - name of the modifier
+params  - parameters of the modifier
+data    - array of data that is available for processing
+options - additional options for scalar modifier
+*/
+function apply(value, name, params, data, options) {
+	options = options || {};
+	switch(name) {
+        case 'circumference':
+            value = 2 * Math.PI * value;
+            break;
+        case 'round':
+            value = Math.round(value);
+            break;
+		case 'htmlcomment':
+        	value = '<!--' + value.toString().toUpperCase() + '-->';
+        	break;
+    }
+
+    return value;
+}
